cba203566286e17a835bb159117a253dc9ba546d
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-01-10 20:44:25 -0600

    added new js function
    added fizz buzz function

--- javascriptFiles/script.js
+++ javascriptFiles/script.js
@@ -3,3 +3,17 @@ function hello () {
 }
 
 hello();
+
+function fizzBuzz () {
+    for(i = 1; i < 100; i++){
+        if(i % 15 === 0 ){
+            console.log("FIZZBUZZ")
+        } else if (i % 3 === 0){
+            console.log("fizz")
+        } else if(i % 5 === 0){
+            console.log("buzz")
+        }
+    }
+}
+
+fizzBuzz();
