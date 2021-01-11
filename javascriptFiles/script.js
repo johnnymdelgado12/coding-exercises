80d596312cc3b2636c97b506e36390260adb4f55
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-01-10 20:48:39 -0600

    corrected spelling error
    corrected spelling error

--- javascriptFiles/script.js
+++ javascriptFiles/script.js
@@ -7,7 +7,7 @@ hello();
 function fizzBuzz () {
     for(i = 1; i < 100; i++){
         if(i % 15 === 0 ){
-            console.log("FIZZBUZZ")
+            console.log("FIZZ BUZZ")
         } else if (i % 3 === 0){
             console.log("fizz")
         } else if(i % 5 === 0){
